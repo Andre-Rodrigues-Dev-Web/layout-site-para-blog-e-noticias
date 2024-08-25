c28f5d2c4523e3629ff3da66d4eb83a9238cd618
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-08-25 13:50:58 -0300

    fix: ajustes de responsividade de title e remoção de sombra sidebar

--- site/assets/js/theme.js
+++ site/assets/js/theme.js
@@ -1,5 +1,6 @@
 //Atributo data-img card notícias topo
-document.querySelectorAll('.card_top--news').forEach(function(element) {
-    var imgUrl = element.getAttribute('data-img');
-    element.style.backgroundImage = 'url(' + imgUrl + ')';
+document.querySelectorAll('.card_top--news').forEach(element => {
+    const imgUrl = element.getAttribute('data-img');
+    element.style.backgroundImage = `url(${imgUrl})`;
 });
+
